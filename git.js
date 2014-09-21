0ed41275ebde5d97eb5884a387963f6c546c5b32
Carl Woodward <carl@88cartell.com>
2014-09-21 22:48:15 +1000

    Add promises version of reading git history.

--- git.js
+++ git.js
@@ -1,4 +1,24 @@
+var Q    = require('q');
 var open = require('nodegit').Repo.open;
+
+// Promises implementation of gitlog.
+
+var deferredOpen = Q.nfcall(open, '.');
+var deferredGetMaster = function(repo) { return Q.ninvoke(repo, 'getMaster') };
+
+deferredOpen.then(function(repo) {
+  return deferredGetMaster(repo);
+}).then(function(branch) {
+  var history = branch.history();
+
+  history.on('commit', function(commit) {
+    console.log(commit.sha(), commit.author(), commit.date(), commit.message());
+  });
+
+  history.start();
+});
+
+// Nested implementation of gitlog.
 
 open('.', function(err, repo) {
   if (err) throw err;
